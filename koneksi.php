dfa4e9c3347075aa56489314112f1dae26ed779e
salvia191002 <93535987+salvia191002@users.noreply.github.com>
2023-10-30 21:19:23 +0700

    backend login

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+$namahost = "localhost";
+$username = "root";
+$password = "";
+$database = "sivalita";
+$conn =
+mysqli_connect($namahost,$username,$password,$database) or die("Failed");
---
+++ process_login.php
@@ -0,0 +1,29 @@
+<?php
+include "koneksi.php";
+session_start();
+
+if ($conn->connect_error) {
+    die("Koneksi database gagal: " . $conn->connect_error);
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Query untuk mencari pengguna dalam tabel login
+    $query = "SELECT * FROM login WHERE username = '$username' AND password = '$password'";
+    $result = $conn->query($query);
+
+    if ($result->num_rows == 1) {
+        // Login berhasil, atur sesi pengguna
+        $_SESSION['username'] = $username;
+        header('Location: index.html'); // Ganti ini dengan halaman setelah login
+        exit();
+    } else {
+        echo "<script>alert('Username atau Password salah!'); window.location='login.html';</script>";
+    }
+}
+
+// Tutup koneksi database
+$conn->close();
+?>
