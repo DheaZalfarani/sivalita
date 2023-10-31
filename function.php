291988f2c4cce17d4ac9282dc0e28c7705af8a14
aritonanq <103364743+aritonanq@users.noreply.github.com>
2023-10-31 15:51:21 +0700

    melengkapi back end

---
+++ function.php
@@ -0,0 +1,256 @@
+<?php
+include "dbconnect.php";
+
+// tambah jadwal shift
+if (isset($_POST['submit'])) {
+    $nama = $_POST["nama"];
+    $tanggal = $_POST["tanggal"];
+    $waktu = $_POST["waktu"];
+    $addtotable = mysqli_query($conn, "INSERT INTO jadwalshift SET
+       nama = '$nama',
+       tanggal = '$tanggal',
+       waktu = '$waktu'
+    ");
+
+    if ($addtotable) {
+        header('location: jadwal-shift.php');
+    } else {
+        echo 'Gagal menambahkan jadwal shift!';
+        header('location: jadwal-shift.php');
+    }
+    ;
+}
+
+
+// edit jadwal shift
+if (isset($_POST['update'])) {
+    $id = $_POST["id"];
+    $nama = $_POST["nama"];
+    $tanggal = $_POST["tanggal"];
+    $waktu = $_POST["waktu"];
+    $updatedata = mysqli_query($conn, "UPDATE jadwalshift SET 
+    nama='$nama', 
+    tanggal='$tanggal', 
+    waktu='$waktu'
+    WHERE id='$id'");
+    if ($updatedata) {
+        echo 'Berhasil memperbarui jadwal shift!';
+
+        header('location: jadwal-shift.php');
+    } else {
+        echo 'Gagal memperbarui jadwal shift!';
+        header('location: jadwal-shift.php');
+    }
+}
+;
+
+// hapus jadwal shift
+if (isset($_POST['hapus'])) {
+    $id = $_POST["id"];
+    $delete = mysqli_query($conn, "DELETE FROM jadwalshift WHERE id='$id'");
+    //hapus juga semua data barang ini di tabel keluar-masuk
+
+    //cek apakah berhasil
+    if ($delete) {
+        header('location: jadwal-shift.php');
+    } else {
+        echo 'Gagal menghapus jadwal shift!';
+        header('location: jadwal-shift.php');
+    }
+}
+;
+
+// tambah jadwal vaksinasi
+if (isset($_POST['submit2'])) {
+    $nama = $_POST["nama"];
+    $nama2 = $_POST["nama2"];
+    $nama3 = $_POST["nama3"];
+    $tanggal = $_POST["tanggal"];
+    $waktu = $_POST["waktu"];
+    $lokasi = $_POST["lokasi"];
+    $jenisvaksin = $_POST["jenisvaksin"];
+    $addtotable = mysqli_query($conn, "INSERT INTO jadwalvaksinasi SET
+        nama='$nama', 
+        nama2='$nama2', 
+        nama3='$nama3', 
+        tanggal='$tanggal', 
+        waktu='$waktu',
+        lokasi ='$lokasi',
+        jenisvaksin='$jenisvaksin'
+    ");
+
+    if ($addtotable) {
+        header('location: jadwal-vaksinasi.php');
+    } else {
+        echo 'Gagal menambahkan jadwal vaksinasi!';
+        header('location: jadwal-vaksinasi.php');
+    }
+    ;
+}
+
+
+// edit jadwal vaksinasi
+if (isset($_POST['update2'])) {
+    $id = $_POST["id"];
+    $nama = $_POST["nama"];
+    $nama2 = $_POST["nama2"];
+    $nama3 = $_POST["nama3"];
+    $tanggal = $_POST["tanggal"];
+    $waktu = $_POST["waktu"];
+    $lokasi = $_POST["lokasi"];
+    $jenisvaksin = $_POST["jenisvaksin"];
+    $updatedata = mysqli_query($conn, "UPDATE jadwalvaksinasi SET 
+    nama='$nama', 
+    nama2='$nama2', 
+    nama3='$nama3', 
+    tanggal='$tanggal', 
+    waktu='$waktu',
+    lokasi ='$lokasi',
+    jenisvaksin='$jenisvaksin'
+    WHERE id='$id'");
+    if ($updatedata) {
+        echo 'Berhasil memperbarui jadwal vaksinasi!';
+
+        header('location: jadwal-vaksinasi.php');
+    } else {
+        echo 'Gagal memperbarui jadwal vaksinasi!';
+        header('location: jadwal-vaksinasi.php');
+    }
+}
+;
+
+// hapus jadwal vaksinasi
+if (isset($_POST['hapus2'])) {
+    $id = $_POST["id"];
+    $delete = mysqli_query($conn, "DELETE FROM jadwalvaksinasi WHERE id='$id'");
+    //hapus juga semua data barang ini di tabel keluar-masuk
+
+    //cek apakah berhasil
+    if ($delete) {
+        header('location: jadwal-vaksinasi.php');
+    } else {
+        echo 'Gagal menghapus jadwal vaksinasi!';
+        header('location: jadwal-vaksinasi.php');
+    }
+}
+;
+
+
+// tambah data vaksinasi
+if (isset($_POST['submit3'])) {
+    $nama = $_POST["nama"];
+    $jenisvaksin = $_POST["jenisvaksin"];
+    $waktu = $_POST["waktu"];
+    $addtotable = mysqli_query($conn, "INSERT INTO vaksinasi SET
+        nama='$nama', 
+        jenisvaksin='$jenisvaksin',
+        waktu='$waktu' 
+    ");
+
+    if ($addtotable) {
+        header('location: data-vaksinasi.php');
+    } else {
+        echo 'Gagal menambahkan data vaksinasi!';
+        header('location: data-vaksinasi.php');
+    }
+    ;
+}
+
+
+// edit data vaksinasi
+if (isset($_POST['update3'])) {
+    $id = $_POST["id"];
+    $nama = $_POST["nama"];
+    $jenisvaksin = $_POST["jenisvaksin"];
+    $waktu = $_POST["waktu"];
+    $updatedata = mysqli_query($conn, "UPDATE vaksinasi SET 
+    nama='$nama', 
+    jenisvaksin='$jenisvaksin',
+    waktu='$waktu'
+    WHERE id='$id'");
+    if ($updatedata) {
+        echo 'Berhasil memperbarui data vaksinasi!';
+
+        header('location: data-vaksinasi.php');
+    } else {
+        echo 'Gagal memperbarui data vaksinasi!';
+        header('location: data-vaksinasi.php');
+    }
+}
+;
+
+// hapus data vaksinasi
+if (isset($_POST['hapus3'])) {
+    $id = $_POST["id"];
+    $delete = mysqli_query($conn, "DELETE FROM vaksinasi WHERE id='$id'");
+    //hapus juga semua data barang ini di tabel keluar-masuk
+
+    //cek apakah berhasil
+    if ($delete) {
+        header('location: data-vaksinasi.php');
+    } else {
+        echo 'Gagal menghapus data vaksinasi!';
+        header('location: data-vaksinasi.php');
+    }
+}
+;
+
+// tambah data vaksin
+if (isset($_POST['submit4'])) {
+    $waktu = $_POST["waktu"];
+    $jenis = $_POST["jenis"];
+    $stok = $_POST["stok"];
+    $addtotable = mysqli_query($conn, "INSERT INTO inventaris SET
+        waktu='$waktu', 
+        jenis='$jenis',
+        stok='$stok' 
+    ");
+
+    if ($addtotable) {
+        header('location: inventaris.php');
+    } else {
+        echo 'Gagal menambahkan data vaksin!';
+        header('location: inventaris.php');
+    }
+    ;
+}
+
+
+// edit data vaksin
+if (isset($_POST['update4'])) {
+    $id = $_POST["id"];
+    $waktu = $_POST["waktu"];
+    $jenis = $_POST["jenis"];
+    $stok = $_POST["stok"];
+    $updatedata = mysqli_query($conn, "UPDATE inventaris SET 
+    waktu='$waktu', 
+    jenis='$jenis',
+    stok='$stok'
+    WHERE id='$id'");
+    if ($updatedata) {
+        echo 'Berhasil memperbarui data vaksin!';
+
+        header('location: inventaris.php');
+    } else {
+        echo 'Gagal memperbarui data vaksin!';
+        header('location: inventaris.php');
+    }
+}
+;
+
+// hapus data vaksin
+if (isset($_POST['hapus4'])) {
+    $id = $_POST["id"];
+    $delete = mysqli_query($conn, "DELETE FROM inventaris WHERE id='$id'");
+    //hapus juga semua data barang ini di tabel keluar-masuk
+
+    //cek apakah berhasil
+    if ($delete) {
+        header('location: inventaris.php');
+    } else {
+        echo 'Gagal menghapus data vaksinasi!';
+        header('location: inventaris.php');
+    }
+}
+;
+?>
---
+++ jadwal-shift.php
@@ -0,0 +1,356 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <!-- import bootstrap  -->
+    <title>Jadwal Shift Petugas Medis</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="assets/img/icon-logo.png" rel="icon">
+
+    <!-- Google Fonts -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- ======= Header ======= -->
+    <header id="header" class="fixed-top">
+        <div class="container d-flex align-items-center">
+            <!-- <h1 class="logo me-auto"><a href="index.html">PHProLabs</a></h1> -->
+            <a href="index.html" class="logo me-auto">SIVALITA</a>
+            <nav id="navbar" class="navbar order-last order-lg-0">
+                <ul>
+                    <li><a href="index.html">Beranda</a></li>
+                    <li><a href="jadwal.html">Jadwal</a></li>
+                    <li><a href="vaksinasi.html">Data Vaksinasi</a></li>
+                    <li><a href="petugas.html">Petugas Medis</a></li>
+                    <li><a href="inventaris.html">Inventaris</a></li>
+                    <li><a href="profil.html"><img src="profil.png" width="25" height="25"></img></a></li>
+                </ul>
+            </nav>
+            <!-- .navbar -->
+        </div>
+    </header>
+    <!-- End Header -->
+    <div id="main" data-bs-aos="fade-in">
+        <!-- breadcrumbs -->
+
+        <div class="breadcrumbs">
+            <div class="container">
+                <h2>Jadwal Shift Petugas Medis</h2>
+                <p> </p>
+            </div>
+        </div>
+        <br><br>
+        <!-- breadcrumbs -->
+        <div class="container" data-bs-aos="fade-up">
+            <div style="text-align: center"><input id="myInput" onkeyup="myFunction()"
+                    style="width:600px; padding: 10px 15px; border-radius: 10px" id="search"
+                    placeholder="Cari jadwal shift.."></input></div>
+            <br><br>
+            <a href="" type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal"
+                style="background-color: #3FBBC0; border-style: none; border-radius: 10px; padding: 10px 15px;color: white">Tambah
+                Jadwal</a><br><br>
+            <br>
+            <!-- tabel -->
+            <table id="myTable" style="width:100%;">
+                <thead class=" thead-dark">
+
+                    <tr>
+                        <th style="width:5%">No.</th>
+                        <th style="width:30%">Nama Petugas</th>
+                        <th style="width:30%">Waktu</th>
+                        <th style="width:10%">Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    // membuat koneksi ke database 
+                    include "dbconnect.php";
+
+                    //membuat variabel angka
+                    $no = 1;
+
+                    //mengambil data dari tabel barang
+                    $jadwal = mysqli_query($conn, "select * from jadwalshift ORDER BY tanggal, waktu");
+
+                    //melooping(perulangan) dengan menggunakan while
+                    while ($data = mysqli_fetch_array($jadwal)) {
+                        $id = $data['id'];
+
+                        ?>
+                        <tr>
+
+                            <!-- menampilkan data dengan menggunakan array  -->
+                            <td>
+                                <?php echo $no++; ?>
+                            </td>
+                            <td>
+                                <?php echo $data['nama']; ?>
+
+                            </td>
+                            <td>
+                                <?php echo $data['tanggal']; ?> -
+                                <?php echo $data['waktu']; ?>
+                            </td>
+
+                            <td>
+
+                                <!-- membuat tombol dengan ukuran small berwarna biru  -->
+                                <!-- data-target setiap modal harus berbeda, karena akan menampilkan data yang berbeda pula
+                                            caranya membedakannya, gunakan id_barang sebagai pembeda data-target di setiap modal -->
+                                <a href="" class="btn btn-sm btn-warning" data-toggle="modal"
+                                    data-target="#edit<?php echo $data['id']; ?>" style="color:white">Edit</a>
+                                <a href="" class="btn btn-sm btn-danger" data-toggle="modal"
+                                    data-target="#hapus<?php echo $data['id']; ?>">Hapus</a>
+                            </td>
+                        </tr>
+
+                        <!-- untuk melihat bentuk-bentuk modal kalian bisa mengunjungi laman bootstrap dan cari modal di kotak pencariannya -->
+                        <!-- id setiap modal juga harus berbeda, cara membedakannya dengan menggunakan id_barang -->
+                        <!-- modal edit -->
+                        <div class="modal fade" id="edit<?php echo $data['id']; ?>" tabindex="-1"
+                            aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Edit Jadwal Shift</h5>
+                                        <button type="button" class="btn-close" data-dismiss="modal"></button>
+                                        </button>
+                                    </div>
+                                    <!-- di dalam modal-body terdapat 4 form input yang berisi data.
+                        data-data tersebut ditampilkan sama seperti menampilkan data pada tabel. -->
+                                    <div>
+                                        <div class="modal-body">
+                                            <form action="function.php" method="post">
+                                                <div class="form-group">
+                                                    <label>Nama Petugas</label>
+                                                    <input name="nama" type="text" class="form-control"
+                                                        placeholder="Nama Petugas Medis"
+                                                        value="<?php echo $data['nama']; ?>">
+                                                </div><br>
+
+                                                <div class="form-group">
+                                                    <label>Tanggal dan Waktu Mulai</label>
+                                                    <input name="tanggal" type="datetime-local" class="form-control"
+                                                        value="<?php echo $data['tanggal']; ?>">
+                                                </div><br>
+                                                <div class="form-group">
+                                                    <label>Waktu Selesai</label>
+                                                    <input name="waktu" type="time" class="form-control"
+                                                        value="<?php echo $data['waktu']; ?>">
+                                                </div>
+                                                <br>
+                                                <input type="hidden" name="id" value="<?= $id; ?>">
+
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-default"
+                                                data-dismiss="modal">Batal</button>
+                                            <input type="submit" name="update" class="btn"
+                                                style="background-color: #3FBBC0; color: white;" value="Simpan">
+                                        </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!--modal edit -->
+                        <!-- modal hapus -->
+                        <div class="modal fade" id="hapus<?php echo $data['id']; ?>" tabindex="-1"
+                            aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <form method="post" action="function.php">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">Hapus Jadwal Shift</h5>
+                                            <button type="button" class="btn-close" data-dismiss="modal"></button>
+                                            </button>
+                                        </div>
+                                        <!-- di dalam modal-body terdapat 4 form input yang berisi data.
+                        data-data tersebut ditampilkan sama seperti menampilkan data pada tabel. -->
+                                        <div class="modal-body">
+                                            Apakah Anda yakin ingin menghapus jadwal shift ini?
+                                            <input type="hidden" name="id" value="<?= $id; ?>">
+                                        </div>
+
+                                        <!-- Modal footer -->
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn" data-dismiss="modal">Batal</button>
+                                            <button type="submit" class="btn btn-danger" name="hapus">Hapus</button>
+                                        </div>
+
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- modal hapus -->
+                    <?php } ?>
+                </tbody>
+            </table>
+
+            <style>
+                table,
+                th,
+                td {
+                    border: 1px solid;
+                    table-layout: auto;
+                    width: 500px;
+                    padding: 2px 2px;
+                    text-align: center;
+                }
+            </style>
+        </div>
+    </div><br><br><br>
+    <!-- ======= Footer ======= -->
+    <footer id="footer">
+        <div class="footer-top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 footer-contact">
+                        <h3>SIVALITA</h3>
+                        <p>
+                            Malang, 2023<br>
+                            Indonesia <br><br>
+                            <strong>Phone:</strong> +62 8888 8888 12<br>
+                            <strong>Email:</strong> sivalita@gmail.com<br>
+                        </p>
+                    </div>
+                    <div class="col-lg-4 col-md-6 footer-links">
+                        <h4>Useful Links</h4>
+                        <ul>
+                            <li><i class="bx bx-chevron-right"></i> <a href="index.html">Beranda</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="jadwal.html">Jadwal</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="vaksinasi.html">Data Vaksinasi</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="petugas.html">Petugas Medis</a></li>
+                            <li><i class="bx bx-chevron-right"></i> <a href="inventaris.html">Inventaris</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container d-md-flex py-4">
+            <div class="me-md-auto text-center text-md-start">
+                <div class="copyright">
+                    &copy; Copyright <strong><span>SIVALITA</span></strong>. All Rights Reserved
+                </div>
+            </div>
+            <div class="social-links text-center text-md-right pt-3 pt-md-0">
+                <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+            </div>
+        </div>
+    </footer>
+    <!-- End Footer -->
+    <!-- Modal Add -->
+    <div id="myModal" class="modal fade">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Tambahk Jadwal Shift</h5>
+                    <button type="button" class="btn-close" data-dismiss="modal"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="function.php" method="post">
+                        <div class="form-group">
+                            <label>Nama Petugas</label>
+                            <input name="nama" type="text" class="form-control" placeholder="Nama Petugas Medis"
+                                value="" required>
+                        </div><br>
+
+                        <div class="form-group">
+                            <label>Tanggal dan Waktu Mulai</label>
+                            <input name="tanggal" type="datetime-local" class="form-control" required>
+                        </div><br>
+                        <div class="form-group">
+                            <label>Waktu Selesai</label>
+                            <input name="waktu" type="time" class="form-control" required>
+                        </div>
+                        <br>
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Batal</button>
+                    <input type="submit" name="submit" class="btn" style="background-color: #3FBBC0; color: white;"
+                        value="Simpan">
+                </div>
+                </form>
+            </div>
+        </div>
+
+    </div>
+    </div>
+    <!-- Modal Add -->
+    <script>
+        function myFunction() {
+            // Declare variables
+            var input, filter, table, tr, td, i, txtValue;
+            input = document.getElementById("myInput");
+            filter = input.value.toUpperCase();
+            table = document.getElementById("myTable");
+            tr = table.getElementsByTagName("tr");
+
+            // Loop through all table rows, and hide those who don't match the search query
+            for (i = 0; i < tr.length; i++) {
+                td = tr[i].getElementsByTagName("td")[1];
+                if (td) {
+                    txtValue = td.textContent || td.innerText;
+                    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                        tr[i].style.display = "";
+                    } else {
+                        tr[i].style.display = "none";
+                    }
+                }
+            }
+        }
+    </script>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+        </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous">
+        </script>
+    <div id="preloader"></div>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+    <!-- Vendor JS Files -->
+    <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/php-email-form/validate.js"></script>
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+    <!-- Start datatable js -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+        </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous">
+        </script>
+</body>
+
+</html>
